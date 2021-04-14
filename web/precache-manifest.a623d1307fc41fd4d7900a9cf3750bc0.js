428c195b8e9647ce0a3dc62c4c21cd8c1632e38a
Naveen <nvnkmrmca@gmail.com>
2021-04-14 14:09:17 +0530

    Web build added.

---
+++ web/precache-manifest.a623d1307fc41fd4d7900a9cf3750bc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adaf6645c4b66401339e0c8a1072ea85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5d6ea38d5641cc86e31",
+    "url": "/static/css/main.f858a48e.chunk.css"
+  },
+  {
+    "revision": "1d50a56cabe64f4bbd5f",
+    "url": "/static/js/2.6fd59d44.chunk.js"
+  },
+  {
+    "revision": "c5d6ea38d5641cc86e31",
+    "url": "/static/js/main.fb22fcd5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
